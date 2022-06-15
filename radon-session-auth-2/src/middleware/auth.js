c40696b7f8637b55ca4e55dcf74c6499e4ba524c
rjtkho <104224077+rjtkho@users.noreply.github.com>
2022-06-15 17:44:32 +0530

    Add files via upload

---
+++ radon-session-auth-2/src/middleware/auth.js
@@ -0,0 +1,48 @@
+const jwt = require("jsonwebtoken");
+
+const authenticate = async function(req, res, next) {
+    //check the token in request header
+    //validate this token
+
+  
+   let token = req.headers["x-Auth-token"];
+    if (!token) token = req.headers["x-auth-token"];
+  
+    //If no token is present in the request header return error
+    if (!token) return res.send({ status: false, msg: "token must be present" });
+  
+    console.log(token);
+    let decodedToken = jwt.verify(token, "functionup-radon");
+    if (!decodedToken)
+      return res.send({ status: false, msg: "token is invalid" });
+     
+    next()
+
+}
+
+const authorise = function(req, res, next) {
+    // comapre the logged in user's id and the id in request
+    
+     let token = req.headers["x-auth-token"]
+    if (!token) token = req.headers["x-auth-token"];
+     if(!token) return res.send({status: false, msg: "token must be present in the request header"})
+    let decodedToken = jwt.verify(token, 'functionup-radon')
+
+    if(!decodedToken) return res.send({status: false, msg:"token is not valid"})
+    
+
+    let userToBeModified = req.params.userId
+    //userId for the logged-in user
+    let userLoggedIn = decodedToken.userId
+
+    //userId comparision to check if the logged-in user is requesting for their own data
+    if(userToBeModified != userLoggedIn) return res.send({status: false, msg: 'User logged is not allowed to modify the requested users data'})
+
+    next()
+}
+
+
+
+
+   module.exports.authenticate = authenticate;
+   module.exports.authorise = authorise;
